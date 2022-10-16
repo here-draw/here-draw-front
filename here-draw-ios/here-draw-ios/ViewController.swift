72d123ff7847f28f47ecd686dda8b240a3a7af33
kizi <phg980513@naver.com>
2022-10-16 13:31:05 +0900

    Del: [#1] Launch.storyboard 삭제

---
+++ here-draw-ios/here-draw-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  here-draw-ios
+//
+//  Created by 박희지 on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
