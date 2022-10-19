27c90f87fce4e010e7e63690f0d67dfa9c0cedea
kizi <phg980513@naver.com>
2022-10-19 15:36:05 +0900

    Add: DeviceUtils 추가
    - 디바이스 크기 정의 util

---
+++ here-draw-ios/here-draw-ios/Util/DeviceUtils.swift
@@ -0,0 +1,39 @@
+//
+//  DeviceUtils.swift
+//  here-draw-ios
+//
+//  Created by 박희지 on 2022/10/18.
+//
+
+import UIKit
+
+// 디바이스 관련 크기 정의
+final class DeviceUtils {
+    
+    // window
+    static let window: UIWindow = {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        return windowScene?.windows.first ?? UIWindow()
+    }()
+    
+    // 디바이스 width
+    static var width: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    // 디바이스 height
+    static var height: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+    // 상태바 height
+    static var statusBarHeight: CGFloat {
+        return  window.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+    }
+    
+    // 네비게이션 바 height
+    static var navigationBarHeight: CGFloat {
+        return UINavigationController().navigationBar.frame.height
+    }
+}
