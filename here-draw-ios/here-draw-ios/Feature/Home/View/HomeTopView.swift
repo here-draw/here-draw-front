a22bfa612ed78e4d16cda6e8a859f84f1d27c649
kizi <phg980513@naver.com>
2022-11-04 02:27:38 +0900

    Feat: [#6] 아티클 배너뷰 구현
    - HomeTopView 수정

--- here-draw-ios/here-draw-ios/Feature/Home/View/HomeTopView.swift
+++ here-draw-ios/here-draw-ios/Feature/Home/View/HomeTopView.swift
@@ -7,14 +7,52 @@
 
 import UIKit
 
-class HomeTopView: UIView {
+import ImageSlideshow
+import Kingfisher
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class HomeTopView: BaseView {
+    
+    var completion: (() -> ())?
+    public var banners: [ImageSource] = [] {
+        didSet {
+            bannerImageView.setImageInputs(banners)
+        }
     }
-    */
+    
+    var pageIndicator: UIPageControl!
+    var bannerImageView: ImageSlideshow!
+    
+    override func setLayout() {
+        pageIndicator = UIPageControl().then {
+            $0.currentPageIndicatorTintColor = .pastelYellow
+            $0.pageIndicatorTintColor = .greyishLightBrown
+            
+            for subview in $0.subviews {
+                subview.snp.remakeConstraints {
+                    $0.width.height.equalTo(10)
+                }
+            }
+        }
+        
+        bannerImageView = ImageSlideshow().then {
+            $0.pageIndicator = pageIndicator
+            $0.pageIndicatorPosition = PageIndicatorPosition(horizontal: .center)
+            $0.pageIndicatorPosition = PageIndicatorPosition(vertical: .bottom)
+            $0.contentScaleMode = .scaleAspectFit
+            $0.slideshowInterval = 3
+            self.addSubview($0)
+            
+            $0.snp.makeConstraints {
+                $0.top.leading.trailing.bottom.equalToSuperview()
+            }
+        }
+    }
+}
 
+extension HomeTopView {
+    @objc func showArticleCompilation() {
+        completion?()
+    }
 }
